41946549639fa4694f716b4f09374dda2f7969a6
henrique expedito <henriqueexpedito964@gmail.com>
2025-04-16 22:16:54 -0300

    Removido README.md e atualizados index.html, script.js e style.css

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let next = document.querySelector('.next');
 let prev = document.querySelector('.prev');
 let slider =document.querySelector('.slider');
@@ -11,18 +10,4 @@ slider.appendChild(slides[0]);
 prev.addEventListener('click', function(){
 let slides = document.querySelectorAll('.slides');
 slider.prepend(slides[slides.length - 1]);
-=======
-let next = document.querySelector('.next');
-let prev = document.querySelector('.prev');
-let slider =document.querySelector('.slider');
-
-next.addEventListener('click', function(){
-let slides = document.querySelectorAll('.slides');
-slider.appendChild(slides[0]);
-})
-
-prev.addEventListener('click', function(){
-let slides = document.querySelectorAll('.slides');
-slider.prepend(slides[slides.length - 1]);
->>>>>>> cf8c516e65ce4c1a9230916b3968100a73eaec65
 })
